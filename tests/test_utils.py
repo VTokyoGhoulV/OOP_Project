2df6923e4d4109f90ac70d352c900d52b61eb162
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-06-12 04:56:00 +0300

    100% test cov

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,6 @@
-from src.utils import from_file_to_classes
+import pytest
+
+from src.utils import from_file_to_classes, find_project_root
 
 
 def test_from_file_to_classes():
@@ -12,3 +14,8 @@ def test_from_file_to_classes():
         == "Смартфоны, как средство не только коммуникации, но и получение дополнительных функций для удобства жизни"
     )
     assert category.products[0].name == "Samsung Galaxy C23 Ultra"
+
+def test_find_project_root_not_found():
+
+    with pytest.raises(RuntimeError):
+        find_project_root("")
