aaa9de39c98230a5c4eff7c00b6f01c8ab62f391
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-06-17 05:46:50 +0300

    add tests (100% cov)

--- tests/test_classes.py
+++ tests/test_classes.py
@@ -1,6 +1,7 @@
 from src.classes import Category, CategoryList, Product
 
 
+# тесты для класса Product
 def test_product_init():
 
     product = Product("Apple", "Simple apple", 50, 100)
@@ -11,6 +12,50 @@ def test_product_init():
     assert product.quantity == 100
 
 
+def test_product_new_product():
+    new_prod = Product.new_product(
+        {
+            "name": "Samsung Galaxy S22 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    assert new_prod.name == "Samsung Galaxy S22 Ultra"
+    assert new_prod.description == "256GB, Серый цвет, 200MP камера"
+    assert new_prod.price == 180000.0
+    assert new_prod.quantity == 5
+
+
+def test_product_new_product_existing_product():
+    new_prod_1 = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5,
+        }
+    )
+    new_prod_2 = Product.new_product(
+        {
+            "name": "Samsung Galaxy S23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 190000.0,
+            "quantity": 5,
+        }
+    )
+    assert new_prod_1.name == "Samsung Galaxy S23 Ultra"
+    assert new_prod_1.description == "256GB, Серый цвет, 200MP камера"
+    assert new_prod_1.price == 180000.0
+    assert new_prod_1.quantity == 5
+
+    assert new_prod_2.name == "Samsung Galaxy S23 Ultra"
+    assert new_prod_2.description == "256GB, Серый цвет, 200MP камера"
+    assert new_prod_2.price == 190000.0
+    assert new_prod_2.quantity == 10
+
+
+# тесты для класса Category
 def test_category_init():
 
     apple = Product("Apple", "Simple apple", 50, 100)
@@ -18,17 +63,16 @@ def test_category_init():
 
     assert category.name == "Фрукты"
     assert category.description == "Свежие фрукты"
-    assert category.products == [apple]
     assert category.category_count == 1
     assert category.product_count == 1
 
 
-def test_category_show_products():
+def test_category_products():
 
     apple = Product("Apple", "Simple apple", 50, 100)
     category = Category("Фрукты", "Свежие фрукты", [apple])
 
-    assert category.show_products() == "Apple - Simple apple - 50 - 100"
+    assert category.products == "Apple, 50 руб. Остаток: 100 шт.\n"
 
 
 def test_category_list_append():
@@ -64,6 +108,69 @@ def test_category_find_by_name_not_found():
     assert not category_list.find_by_name("Яблоки")
 
 
+def test_category_add_product():
+
+    apple = Product("Apple", "Simple apple", 50, 100)
+    category = Category("Фрукты", "Свежие фрукты", [apple])
+
+    category.add_product(Product("Phone", "Simple phone", 500, 10))
+
+    assert category.products == "Apple, 50 руб. Остаток: 100 шт.\nPhone, 500 руб. Остаток: 10 шт.\n"
+
+
+def test_price_getter():
+    product = Product("Apple", "Simple apple", 50, 100)
+    assert product.price == 50
+
+
+def test_price_setter():
+    product = Product("Apple", "Simple apple", 50, 100)
+    product.price = 100
+    assert product.price == 100
+
+
+def test_price_setter_negative(capsys):
+    product = Product("Apple", "Simple apple", 50, 100)
+    product.price = -100
+    assert capsys.readouterr().out == "Цена не должна быть нулевая или отрицательная\n"
+    assert product.price == 50
+
+
+def test_price_setter_zero(capsys):
+    product = Product("Apple", "Simple apple", 50, 100)
+    product.price = 0
+    assert capsys.readouterr().out == "Цена не должна быть нулевая или отрицательная\n"
+    assert product.price == 50
+
+
+def test_price_setter_lower_than_current_yes(monkeypatch):
+    product = Product("Apple", "Simple apple", 50, 100)
+
+    monkeypatch.setattr("builtins.input", lambda _: "Y")
+
+    product.price = 40
+
+    assert product.price == 40
+
+
+def test_price_setter_lower_than_current_no(monkeypatch):
+    product = Product("Apple", "Simple apple", 50, 100)
+
+    monkeypatch.setattr("builtins.input", lambda _: "N")
+
+    product.price = 40
+
+    assert product.price == 50
+
+
+def test_price_setter_higher_than_current():
+    product = Product("Apple", "Simple apple", 50, 100)
+    product.price = 60
+
+    assert product.price == 60
+
+
+# тесты для класса CategoryList
 def test_category_list_get_all_categories():
 
     apple = Product("Apple", "Simple apple", 50, 100)
