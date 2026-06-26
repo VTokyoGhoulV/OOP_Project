4c4f6bd50bc29f72acbb94fb86c1384af345abae
-TokyoGhoul- <0hellnomad0@gmail.com>
2026-06-26 19:13:38 +0300

    small fixes (linters)

--- src/utils.py
+++ src/utils.py
@@ -9,9 +9,7 @@ def from_file_to_classes() -> CategoryList:
     Преобразует данные из JSON файла в классы
     """
 
-    with open(
-        f"{find_project_root()}/data/products.json", "r", encoding="utf-8"
-    ) as file:
+    with open(f"{find_project_root()}/data/products.json", "r", encoding="utf-8") as file:
         categories = json.load(file)
 
     category_list = CategoryList()
@@ -35,9 +33,7 @@ def from_file_to_classes() -> CategoryList:
     return category_list
 
 
-def find_project_root(
-    marker_files: str | tuple = ("pyproject.toml", ".git", "requirements.txt")
-) -> Path:
+def find_project_root(marker_files: str | tuple = ("pyproject.toml", ".git", "requirements.txt")) -> Path:
     """
     Ищет корневую директорию проекта, поднимаясь по дереву папок,
     пока не найдет один из маркерных файлов/папок.
@@ -49,6 +45,4 @@ def find_project_root(
             if (parent / marker).exists():
                 return parent
 
-    raise RuntimeError(
-        "Не удалось найти корень проекта. Убедитесь, что один из маркерных файлов присутствует."
-    )
+    raise RuntimeError("Не удалось найти корень проекта. Убедитесь, что один из маркерных файлов присутствует.")
